838ad4ade616a8e1f0ac7d0d0957d5aff1ca3144
George Zakhour <g1995z@gmail.com>
2018-02-02 18:33:37 +0100

    Added support for search

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,13 @@ var app = new Vue({
     el: "#app",
     data: {
         notes: [],
+        displayedNotes: [],
         selectedNote: null,
         selectedNoteTag: "",
         emptyNote: { title: "", tags: [], id: 0, versions: [] },
         editView: true,
+
+        searchQuery: "",
 
         autoSave: {
             saving: false,
@@ -70,8 +73,8 @@ var app = new Vue({
 
         createNewNote: function() {
             var newNote = {
-                id: this.notes.map(function(note) { return note.id; })
-                        .reduce(function(a, b) { return Math.max(a, b) })+1,
+                id: this.notes.map(note => note.id)
+                        .reduce(a, b => Math.max(a, b))+1,
                 title: "",
                 tags: [],
                 versions: []
@@ -83,9 +86,7 @@ var app = new Vue({
 
         deleteSelectedNote: function() {
             var self = this;
-            this.notes = this.notes.filter(function(note) {
-                return note.id !== self.selectedNote.id;
-            });
+            this.notes = this.notes.filter(note => note.id !== self.selectedNote.id);
             if (this.notes.length == 0) {
                 this.selectedNote = this.emptyNote;
             } else {
@@ -178,6 +179,72 @@ var app = new Vue({
                 else return (new Date(b.versions[0].createdAt)).valueOf() -
                             (new Date(a.versions[0].createdAt)).valueOf();
             });
+        },
+
+        queryNotes: function() {
+            if (this.searchQuery == 0) {
+                this.displayedNotes = this.notes;
+                return;
+            }
+
+            var query = this.searchQuery.toLowerCase();
+            var tokens = query.split(' ');
+
+            var requiredTags = [];
+            var incompleteTag = "";
+            var requiredTitle = [];
+            var requiredBody = [];
+            var otherTokens = [];
+
+            var bodyExtractor = note => note.versions[0].body
+
+            // the user could be in the process of typing, the last token
+            // should not be as strict to that effect
+            var lastToken = tokens.slice(-1)[0];
+            if (lastToken.startsWith("tag:")) {
+                incompleteTag = lastToken.substring(lastToken.indexOf(':')+1);
+                tokens.pop();
+            } else if (["tag", "title", "body"].some(qc => qc.startsWith(lastToken))) {
+                tokens.pop();
+            }
+
+            tokens.forEach(token => {
+                token = token.toLowerCase();
+                if (token.startsWith("tag:")) {
+                    requiredTags.push(token.substring(4));
+                } else if (token.startsWith("title:")) {
+                    requiredTitle.push(token.substring(6));
+                } else if (token.startsWith("body:")) {
+                    requiredBody.push(token.substring(5));
+                } else if (token.length >= 3) {
+                    otherTokens.push(token);
+                }
+            });
+
+            this.displayedNotes = this.notes.filter(note => {
+                // I assume the law of the excluded middle in all conditions
+                // all explicit tags (in requiredTags) should exist
+                if (requiredTags.some(tag => !note.tags.includes(tag))
+                 || note.tags.every(tag => !tag.startsWith(incompleteTag)))
+                    return false;
+                // condition that fails a field for not containing a message
+                var cond = field => msg => field(note).toLowerCase().indexOf(msg) > -1;
+                var failedCond = field => msg => !cond(field)(msg);
+                // all explicit titles should be part of the title
+                if (requiredTitle.some(failedCond(note => note.title)))
+                    return false;
+                // all explicit body parts should be contained in the body
+                if (requiredBody.some(failedCond(note => note.versions[0].body)))
+                    return false;
+                // other tokens are used to search in title, tags, or body
+                // and not all are required to exist
+                if (otherTokens.length == 0)
+                    return true;
+                return otherTokens.some(token => 
+                            cond(bodyExtractor)(token)
+                         || cond(note => note.title)(token)
+                         || note.tags.some(tag => tag.startsWith(token)))
+            });
         }
     },
     beforeMount: function() {
@@ -185,6 +252,7 @@ var app = new Vue({
         if (this.notes.length > 0) {
             this.sortNotes();
             this.selectNote(this.notes[0]);
+            this.displayedNotes = this.notes;
         } else {
             this.selectNote(this.emptyNote);
         }
