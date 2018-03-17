378684ac94d382deb72a4b72e03e8103ecf0ed85
George Zakhour <g1995z@gmail.com>
2018-03-17 17:08:55 +0100

    Added a plugin that evaluates javascript in a note and displays output

--- resources/note-format/note-format.out.js
+++ resources/note-format/note-format.out.js
@@ -82,4 +82,26 @@ const formatters=[function (body) {
 
     return Promise.resolve(sandbox.innerHTML);
 }
+,function (body, note) {
+    const sandbox = document.createElement('div');
+    sandbox.innerHTML = body;
+
+    Array.from(sandbox.querySelectorAll('code.js')).forEach(elm => {
+        const escaper = document.createElement('textarea');
+        escaper.innerHTML = elm.innerHTML;
+
+        if (escaper.value.split('\n')[0].trim() !== '!eval') {
+            return;
+        }
+
+        const iframe = document.createElement('iframe');
+        iframe.sandbox = 'allow-scripts';
+        iframe.className = 'javascript';
+        iframe.srcdoc = '<script>' + escaper.value + '</script>';
+
+        elm.parentNode.replaceChild(iframe, elm);
+    });
+
+    return Promise.resolve(sandbox.innerHTML);
+}
 ,]
